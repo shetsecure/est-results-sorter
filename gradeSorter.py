63572069b160cefe31ac447df5ca8ca1c84a16ee
Amine Marzouki <shetsecure@gmail.com>
2019-01-06 19:43:39 +0100

    Handling file not found error

--- gradeSorter.py
+++ gradeSorter.py
@@ -1,67 +1,98 @@
 # - *- coding: utf- 8 - *-
-
-# MADE WITH LOVE BY AMINE MARZOUKI
-
 import re
-import operator
+import os.path
 
-filename = raw_input('Enter the filename: ')
-module_name = [ line for line in open(filename, 'r') if 'Elément pédagogique' in line]
-if (len(module_name) > 0):
-	print(module_name[0])
-num_lines = sum(1 for line in open(filename))
+def menu():
+	print("ESTF results Sorter")
+	print("What you want to do ?")
+	print("[1] : Sort one CSV file")
+	print("[2] : Sort multiple CSV files and merge the results")
 
-# getting number of subjects in the module
-ff = open(filename, 'r')
-all_text = ''.join(ff.readlines())
-num_u_p = int(all_text.count("CNE")) # number of CNE in the file = number of pages. It's used to calculate the
-									 # number of subjects properly
-num_subj = int(all_text.count("Ado / 20")/num_u_p) - 1 # number of subjects -> will be used to solve the name problem
 
-f = open(filename, 'r') # to get all text after CNE line (the beginning of the students list)
-for i in range(1,num_lines):
-	if(f.readline().find("CNE") >= 0):
-		res = f.readlines()
-		break
+	good = 0
 
-length = len(res)
+	while not good:
+		try:
+			choice = int(raw_input("Your choice: "))
+			if choice == 1 or choice == 2:
+				good = 1
+			else:
+				print("Please choose 1 or 2")
+		except ValueError, e:
+			print ("%s is not a valid integer." % e.args[0].split(": ")[1])
 
-for i in range(0,length):
-	if(res[i].find("Nombre total d'étudiants : ") > 0):
-		nString = res[i]
-		n = re.findall(r'\d+', nString)[0] # number of students
-		break # found it, get out
+	return choice
 
-resultList = [] # the list that will contain the results
-n = int(n)
-j = 0
-bad = False # bool var to check if the string matchs the form of CNE
-k = 0
+def sortByGrades(path):
+	module_name = [ line for line in open(filename, 'r') if 'Elément pédagogique' in line]
+	if (len(module_name) > 0):
+		print(module_name[0])
+	num_lines = sum(1 for line in open(filename))
 
-while k <= n:
-	try:
-		l = res[j].split('"')[1].split()
-		ll = []
-		bad = False
-		j += 1
-		if(l):
-			if re.match(r"^(([A-Z]|[0-9])[0-9]{9})$", l[0]):
-				k += 1
-				for i in range(3, len(l)-num_subj):
-					ll.append(l[i])
-				resultList.append(ll)
+	# getting number of subjects in the module
+	ff = open(filename, 'r')
+	all_text = ''.join(ff.readlines())
+	num_u_p = int(all_text.count("CNE")) # number of CNE in the file = number of pages. It's used to calculate the
+										 # number of subjects properly
+	num_subj = int(all_text.count("Ado / 20")/num_u_p) - 1 # number of subjects -> will be used to solve the name problem
+
+	f = open(filename, 'r') # to get all text after CNE line (the beginning of the students list)
+	for i in range(1,num_lines):
+		if(f.readline().find("CNE") >= 0):
+			res = f.readlines()
+			break
+
+	length = len(res)
+
+	for i in range(0,length):
+		if(res[i].find("Nombre total d'étudiants : ") > 0):
+			nString = res[i]
+			n = re.findall(r'\d+', nString)[0] # number of students
+			break # found it, get out
+
+	resultList = [] # the list that will contain the results
+	n = int(n)
+	j = 0
+	bad = False # bool var to check if the string matchs the form of CNE
+	k = 0
+
+	while k <= n:
+		try:
+			l = res[j].split('"')[1].split()
+			ll = []
+			bad = False
+			j += 1
+			if(l):
+				if re.match(r"^(([A-Z]|[0-9])[0-9]{9})$", l[0]):
+					k += 1
+					for i in range(3, len(l)-num_subj):
+						ll.append(l[i])
+					resultList.append(ll)
+				else:
+					bad = True
+					continue
 			else:
 				bad = True
 				continue
-		else:
-			bad = True
-			continue
-		if(bad):
-			j -= 1
-	except IndexError:
-		break
+			if(bad):
+				j -= 1
+		except IndexError:
+			break
 
-resultList.sort(key = lambda x : float(x[len(x)-2]), reverse=True)
+	resultList.sort(key = lambda x : float(x[len(x)-2]), reverse=True)
 
-for i in range(0,n):
-	print(str(i+1).zfill(2) + ': ' + ' '.join([str(x) for x in resultList[i]]))
+	for i in range(0,n):
+		print(str(i+1).zfill(2) + ': ' + ' '.join([str(x) for x in resultList[i]]))
+
+
+
+choice = menu()
+
+if choice == 1:
+	filename = raw_input('Enter the filename: ')
+	if os.path.exists(filename):
+		sortByGrades(filename)
+	else:
+		print("File Not Found")
+#filename = raw_input('Enter the filename: ')
+#sortByGrades(filename)
